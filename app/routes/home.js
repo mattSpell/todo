b2e90f7b29f7d173509c5d823ecdaf5759e101e9
Matt Spell <matthew.spell1@gmail.com>
2014-05-28 11:55:13 -0500

    done

--- app/routes/home.js
+++ app/routes/home.js
@@ -1,7 +1,7 @@
 'use strict';
 
 exports.index = (req, res)=>{
-  res.render('home/index', {title: 'Node.js: Home'});
+  res.render('home/index', {bg: 'homebg.jpg', title: 'Todo List: Home'});
 };
 
 exports.help = (req, res)=>{
